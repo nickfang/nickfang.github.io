483bafc39bc1979f02a1de3432189be837685f0d
Nick Fang <fang.nicholas@gmail.com>
2024-06-02 13:26:00 -0500

    updating descriptions.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const main = () => {
-	return (
-		<div>
-			<h1>Nicholas Fang Dev</h1>
-		</div>
-	);
+  return (
+    <div>
+      <p>I am a full stack software engineer that builds web apps.</p>
+    </div>
+  );
 };
 
 export default main;
--- src/components/main-header/main-header.js
+++ src/components/main-header/main-header.js
@@ -13,7 +13,7 @@ export default function MainHeader() {
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
           {/* <Image src={logoImg} alt="A plate with food on it." priority/> */}
-          Nicholas Fang Dev
+          Nicholas Fang <span>&#9675;</span> Dev
         </Link>
         <nav className={classes.nav}>
           <ul>
