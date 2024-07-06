23ec6b2dbbeb0dd8adce894ed6db0f2d40d4f2bb
Nick Fang <fang.nicholas@gmail.com>
2024-07-05 21:36:30 -0500

    Another try to deploy.

--- src/components/display/animatedIcon.tsx
+++ src/components/display/animatedIcon.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { GlobeIcon } from '@/components/display/Icons';
 
 const AnimatedIcon = () => {
-  return (
-    <GlobeIcon className="transition-transform duration-300 ease-in-out hover:scale-0 hover:scale-100" />
-  );
+  return <GlobeIcon className="transition-transform duration-300 ease-in-out hover:scale-100" />;
 };
 
 export default AnimatedIcon;
