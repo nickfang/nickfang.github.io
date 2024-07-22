d9e003be960b7ce5136a0950e4c2aba3487cd864
Nick Fang <fang.nicholas@gmail.com>
2024-07-22 11:49:45 -0500

    Adding a page for playing around with calculating colors for backgrounds.

---
+++ src/app/ideas/styling/color/calc-color/page.tsx
@@ -0,0 +1,49 @@
+import styles from './page.module.css';
+
+const CalcColor = () => {
+  return (
+    <div className={styles.base}>
+      <p>
+        Setting shades of backgrounds programatically using oklch for the background and
+        color-contrast for the text.
+      </p>
+      <div className={(styles.bg, styles.dark)}>
+        Box
+        <div className={styles['bg-lighten']}>
+          Lighter box
+          <div className={styles['bg-lighten']}>
+            Even lighter
+            <div className={styles['bg-lighten']}>Even lighter</div>
+          </div>
+        </div>
+        <div className={styles['bg-darken']}>
+          Darker box
+          <div className={styles['bg-darken']}>
+            Even darker
+            <div className={styles['bg-darken']}>Even darker</div>
+          </div>
+        </div>
+      </div>
+
+      <div className={(styles.bg, styles.light)}>
+        Box
+        <div className={styles['bg-lighten']}>
+          Lighter box
+          <div className={styles['bg-lighten']}>
+            Even lighter
+            <div className={styles['bg-lighten']}>Even lighter</div>
+          </div>
+        </div>
+        <div className={styles['bg-darken']}>
+          Darker box
+          <div className={styles['bg-darken']}>
+            Even darker
+            <div className={styles['bg-darken']}>Even darker</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalcColor;
