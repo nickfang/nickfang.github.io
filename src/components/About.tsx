6dd0f1b158306293199584ff301ea4b3dfbf3fef
Nick Fang <fang.nicholas@gmail.com>
2024-08-07 14:17:24 -0500

    Adding links for github and linkedin.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,6 +5,12 @@ const About = () => {
     <div className="m-8">
       <h1 className="m-8">About Me</h1>
       <p className="px-8">I am a full-stack software engineer with a passion for building innovative web applications. My core competencies lie in JavaScript, React, and Node.js, but I am continually expanding my skill set. Currently, I am exploring Rust and Go, while also delving into the realm of 3D printing. With a proven track record in the SaaS industry, I have a strong foundation in delivering user-centric products.</p>
+      <div className="my-8 mx-16">
+        <ul>
+          <li><a href="https://github.com/nickfang">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/nicholasfang">LinkedIn</a></li>
+        </ul>
+      </div>
     </div>
   );
 };
