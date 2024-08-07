d7d09b6b7b599683f17a11f715a8f967759a8a56
Nick Fang <fang.nicholas@gmail.com>
2024-08-07 11:06:11 -0500

    Updating about page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const main = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>I am a full stack software engineer that builds web apps.</p>
+    <div className="m-4">
+      <h1 className="mb-8">About Me</h1>
+      <p className="px-8">I am a full-stack software engineer with a passion for building innovative web applications. My core competencies lie in JavaScript, React, and Node.js, but I am continually expanding my skill set. Currently, I am exploring Rust and Go, while also delving into the realm of 3D printing. With a proven track record in the SaaS industry, I have a strong foundation in delivering user-centric products.</p>
     </div>
   );
 };
