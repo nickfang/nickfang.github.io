27508818deb841815b8537a716f0a312460cd991
Nick Fang <fang.nicholas@gmail.com>
2024-08-16 12:58:24 -0500

    Added the matrix style canvas to canvas page.

--- src/app/ideas/canvas/page.tsx
+++ src/app/ideas/canvas/page.tsx
@@ -9,11 +9,12 @@ const CanvasCreations = () => {
     { name: 'Line', path: '/ideas/canvas/lines', filename: '/images/canvas-lines.png' },
     { name: 'Connected', path: '/ideas/canvas/connected', filename: '/images/canvas-connected.png' },
     { name: 'Typography', path: '/ideas/canvas/typography', filename: '/images/canvas-typography.png' },
+    { name: 'Matrix', path: '/ideas/canvas/matrix', filename: '/images/canvas-matrix.png' },
   ]
   return (
     <div className="m-8">
       <h1 className="m-8">Canvas Creations</h1>
-      <p className="px-8">This is a collection of canvas experiments that I am playing with. Each page contains a unique canvas element. I hope you enjoy exploring these creations!</p>
+      <p className="px-8">This is a collection of canvas experiments/tutorials that I am playing with. Each page contains a unique canvas element. I hope you enjoy exploring these creations!</p>
       <div className="flex flex-wrap justify-center align-middle">
         {canvases.map((canvas, index) => (
           <div key={index} className="my-8 mx-4">
